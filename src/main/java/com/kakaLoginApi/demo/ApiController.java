94e5025cfc3c21efe0e6f849c8a97d8519435af0
Hongrokgi <Hongrokgi@naver.com>
2022-09-07 17:18:50 +0900

    create Repository 20220907_17:18

---
+++ src/main/java/com/kakaLoginApi/demo/ApiController.java
@@ -0,0 +1,14 @@
+package com.kakaLoginApi.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.http.HttpRequest;
+
+@RestController
+@Slf4j
+public class ApiController {
+
+}
